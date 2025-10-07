8ddcf4fa81b738e3237df2e83e0b939d83de9298
PaarthKulkarni <paarthkiran1996@gmail.com>
2025-10-06 21:40:22 -0400

    Hopefully fixes Vercel issue

--- src/app/(Kambaz)/Account/Layout.tsx
+++ src/app/(Kambaz)/Account/Layout.tsx
@@ -1,21 +1,19 @@
 import { ReactNode } from "react";
 import AccountNavigation from "./Navigation";
-import { FaAlignJustify } from "react-icons/fa";
-
 export default function AccountLayout({ children }: Readonly<{ children: ReactNode }>) {
  return (
-
-      <div id="wd-courses">
-        <br/>
-        <div className="d-flex">
-        <div className="d-none d-md-block">
-        <AccountNavigation />
-        </div>
-      <div className="flex-fill">
-        {children}
-        </div>
-            </div>
-            </div>
-
-  
+   <div id="wd-kambaz">
+     <table>
+       <tbody>
+         <tr>
+           <td valign="top">
+             <AccountNavigation />
+           </td>
+           <td valign="top" width="100%">
+             {children}
+           </td>
+         </tr>
+       </tbody>
+     </table>
+  </div>
 );}
