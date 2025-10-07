7025587056ac8dec525ee191d9b8144abb985992
PaarthKulkarni <paarthkiran1996@gmail.com>
2025-10-06 20:52:43 -0400

    Hopefully fixes Vercel issue

--- src/app/(Kambaz)/Account/Navigation.tsx
+++ src/app/(Kambaz)/Account/Navigation.tsx
@@ -7,17 +7,14 @@ export default function AccountNavigation() {
  return (
       <div id="wd-accounts-navigation"  className="wd list-group fs-5 rounded-0"> 
       <Link href="Signin" id="wd-account-link" className={`${path.includes("Signin") ? "text-black" : "text-danger"} text-decoration-none`}>
-         <br />
          {path.includes("Signin") && <RxDividerVertical size={24} className="me-1 fs-1 text-black"/>}
          Signin
        </Link>
       <Link href="Signup" id="wd-account-link" className={`${path.includes("Signup") ? "text-black" : "text-danger"} text-decoration-none`}>
-         <br />
          {path.includes("Signup") && <RxDividerVertical size={24} className="me-1 fs-1 text-black"/>}
          Signup
        </Link>
       <Link href="Profile" id="wd-account-link" className={`${path.includes("Profile") ? "text-black" : "text-danger"} text-decoration-none`}>
-         <br />
          {path.includes("Profile") && <RxDividerVertical size={24} className="me-1 fs-1 text-black"/>}
          Profile
        </Link>
