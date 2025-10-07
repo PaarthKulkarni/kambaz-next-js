97c40883c04d64cb3880efaa1dcd913ef4543549
PaarthKulkarni <paarthkiran1996@gmail.com>
2025-10-06 21:03:37 -0400

    Hopefully fixes Vercel issue

--- src/app/(Kambaz)/Account/Layout.tsx
+++ src/app/(Kambaz)/Account/Layout.tsx
@@ -1,15 +1,21 @@
 import { ReactNode } from "react";
 import AccountNavigation from "./Navigation";
+import { FaAlignJustify } from "react-icons/fa";
+
 export default function AccountLayout({ children }: Readonly<{ children: ReactNode }>) {
  return (
-   <div id="wd-kambaz">
-    <div className="d-flex">
-      <div>
-             <AccountNavigation />
-             </div>
-           <div className="wd-main-content-offset p-3 flex-fill">
-             {children}
+
+      <div id="wd-courses">
+        <br/>
+        <div className="d-flex">
+        <div className="d-none d-md-block">
+        <AccountNavigation />
+        </div>
+      <div className="flex-fill">
+        {children}
+        </div>
             </div>
-     </div>
-  </div>
+            </div>
+
+  
 );}
--- src/app/(Kambaz)/Account/Navigation.tsx
+++ src/app/(Kambaz)/Account/Navigation.tsx
@@ -6,17 +6,8 @@ export default function AccountNavigation() {
   const path = usePathname();
  return (
       <div id="wd-accounts-navigation"  className="wd list-group fs-5 rounded-0"> 
-      <Link href="Signin" id="wd-account-link" className={`${path.includes("Signin") ? "text-black" : "text-danger"} text-decoration-none`}>
-         {path.includes("Signin") && <RxDividerVertical size={24} className="me-1 fs-1 text-black"/>}
-         Signin
-       </Link>
-      <Link href="Signup" id="wd-account-link" className={`${path.includes("Signup") ? "text-black" : "text-danger"} text-decoration-none`}>
-         {path.includes("Signup") && <RxDividerVertical size={24} className="me-1 fs-1 text-black"/>}
-         Signup
-       </Link>
-      <Link href="Profile" id="wd-account-link" className={`${path.includes("Profile") ? "text-black" : "text-danger"} text-decoration-none`}>
-         {path.includes("Profile") && <RxDividerVertical size={24} className="me-1 fs-1 text-black"/>}
-         Profile
-       </Link>
+      <Link href="Signin" id="wd-account-link" className={`list-group-item border-0 ${path.includes("Signin") ? "active" : "text-danger"}`}>Signin</Link>
+      <Link href="Signup" id="wd-account-link" className={`list-group-item border-0 ${path.includes("Signup") ? "active" : "text-danger"}`}>Signup</Link>
+      <Link href="Profile" id="wd-account-link" className={`list-group-item border-0 ${path.includes("Profile") ? "active" : "text-danger"}`}>Profile</Link>
     </div>
 );}
